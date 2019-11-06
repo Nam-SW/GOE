4e3f20e289f39ed591ce3103b91129ca6f514a9d
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-11-06 20:24:05 +0900

    데이터 수집 프레임워크 완성. 이제 이걸 총괄하는 파일 하나 만들고 npy로 저장하면 끝

--- AI/dataset/weather_data.py
+++ AI/dataset/weather_data.py
@@ -14,18 +14,22 @@ def get_url(year, month, option):
         url += '&obs=9&x=0&y=0'
     return url
 
-def get_temperature(year, month):
+def get_temperature(year):
     '''
-    월별로 온도 구하는 함수임
+    년별로 온도 구하는 함수임
     '''
-    temperature = {}
+    temperature = []
 
-    res = requests.get(get_url(year, month, 1))
-    soup = BeautifulSoup(res.content, 'html5lib')
-    find = soup.find_all(string=re.compile('평균기온:'))
+    for month in range(1, 13):
+        temp = []
+        res = requests.get(get_url(year, month, 1))
+        soup = BeautifulSoup(res.content, 'html5lib')
+        find = soup.find_all(string=re.compile('평균기온:'))
 
-    for i, j in zip([i+1 for i in range(len(find))], find):
-        temperature[i] = float(j[5:-1])
+        for i in find:
+            temp.append(float(i[5:-1]))
+        
+        temperature.append(temp)
 
     return temperature
 
@@ -36,38 +40,70 @@ def get_day(year, month, day):
     day_code = [2, 3, 4, 5, 6, 7, 1]
     return day_code[datetime.date(year, month, day).weekday()]
 
-
-def get_weather_list(year):
+def get_weather_code(year):
     '''
-    일단 이 함수는 예측된 날씨들이 뭐뭐가 있는지 내가 알기 위해 사용하는거.
-    어차피 이런 날씨들은 내가 직접 코드화할거라 프로그래밍에 영향을 끼치진 않음
-    단 안되면 내가 페이지 하나하나 방문하면서 뭐뭐있는지 알아야하기 때문에 내가 빡침
+    년도를 받아와서 그 년도의 데이터를 측정함. 
+    19년 데이터는 올바르지 않으니 그 이전 데이터를 쓰도록 하자.
     '''
-    weather = []
+    weather_list, weather_code = [], []
+    
     for i in range(1, 13):
+        temp = []
         res = requests.get(get_url(year, i, 2))
         soup = BeautifulSoup(res.content, 'html5lib')
         find = soup.find_all(class_='align_left')
 
-        idx = get_day(year, i, 1) + 6
+        # 월별로 시작하는 요일
+        start = get_day(year, i, 1) + 6
 
-        for i in find[idx:]:
-            text = i.get_text()
-            if not text[0].isdigit():
-                weather.append(text)
-                # print(text)
+        for j in range(start, len(find)):
+            text = find[j].get_text()
+            if find[j-7].get_text()[0].isdigit():
+                temp.append(text)
+        
+        weather_list.append(temp)
 
-    # weather = list(set(weather))
-    weather_str = ''
-    for i in weather:
-        weather_str += i
+    # 코드화
+    for weather in weather_list:
+        temp = []
+        for i in weather:
+            if '진눈깨비' in i:
+                # 진눈깨비: 5
+                temp.append(5)
+            elif '눈' in i:
+                # 눈: 4
+                temp.append(4)
+            elif '소나기' in i or '비' in i or '우박' in i:
+                # 비: 3
+                temp.append(3)
+            elif '무' in i or '운' in i or '안개' in i:
+                # 흐림: 2
+                temp.append(2)
+            elif '\xa0' in i or '햇무리' in i:
+                # 맑음: 1
+                temp.append(1)
+            else:
+                # 아직 코드를 추가하지 않음. 나중에 프로그램이 커지면 알아서 하겠지
+                temp.append(0)
+        weather_code.append(temp)
     
-
+    ''' 자연어처리
+    # weather = list(set(weather))
+    # weather_str = ''
+    # for i in weather:
+    #     weather_str += i
     # h = Twitter()
     # nouns = h.nouns(weather_str)
-    return weather
+    '''
+    return weather_code
 
-pprint(get_weather_list(2018))
+
+if __name__ == '__main__':
+    # for i in get_weather_code(2018):
+    for i, j in zip(get_temperature(2018), get_weather_code(2018)):
+        print(i)
+        print(j)
+        print(len(i), len(j))
 
 
 """
--- AI/dataset/weather_predict.py
+++ AI/dataset/weather_predict.py
@@ -1,55 +1,59 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 from pprint import pprint
 
-# 드라이버 생성
-# chromedriver 설치된 경로를 정확히 기재해야 함
-chromedriver = 'C:\\ProgramData\\Anaconda3\\chromedriver.exe' # 윈도우 
-url = 'https://www.accuweather.com/ko/kr/seoul/226081/daily-weather-forecast/226081?day='
-driver = webdriver.Chrome(chromedriver)
+def get_predict_code(length=30):
+    '''
+    지금으로부터 한달간의 온도와 날씨정보를 튜플형태로 출력.
+    그냥 지금으로부터 x일 후까지를 원하면 x값을 넣으면 됨
+    '''
+    chromedriver = 'C:\\ProgramData\\Anaconda3\\chromedriver.exe' # 윈도우 
+    url = 'https://www.accuweather.com/ko/kr/seoul/226081/daily-weather-forecast/226081?day='
+    driver = webdriver.Chrome(chromedriver)
 
-temperature_list = []
-weather_list = []
+    temperature_list = []
+    weather_list = []
 
-for i in range(1, 81):
-    driver.get(url + str(i))
+    for i in range(1, length):
+        driver.get(url + str(i))
 
-    t = driver.find_elements_by_class_name('value')
-    w = driver.find_elements_by_class_name('phrase')
+        t = driver.find_elements_by_class_name('value')
+        w = driver.find_elements_by_class_name('phrase')
 
-    high, low = str(t[0].text), str(t[1].text)
-    mean = (int(high[:high.index('°')]) + int(low[:low.index('°')])) / 2
-    temperature_list.append(mean)
-    weather_list.append((str(w[0].text), str(w[1].text)))
+        high, low = str(t[0].text), str(t[1].text)
+        mean = (int(high[:high.index('°')]) + int(low[:low.index('°')])) / 2
+        temperature_list.append(mean)
+        weather_list.append((str(w[0].text), str(w[1].text)))
 
-driver.quit()
+    driver.quit()
 
-# pprint(temperature_list)
-weather_code = []
-for weather in weather_list:
-    i = weather[0]
-    if '진눈깨비' in i:
-        # 진눈깨비: 5
-        weather_code.append(5)
-    elif '눈' in i:
-        # 눈: 4
-        weather_code.append(4)
-    elif '소나기' in i or '비' in i:
-        # 비: 3
-        weather_code.append(3)
-    elif '흐' in i:
-        # 흐림: 2
-        weather_code.append(2)
-    elif '맑' in i or '줄어듦' in i:
-        # 맑음: 1
-        weather_code.append(1)
-    else:
-        # 아직 코드를 추가하지 않음. 나중에 프로그램이 커지면 알아서 하겠지
-        weather_code.append(0)
+    weather_code = []
+    for weather in weather_list:
+        i = weather[0]
+        if '진눈깨비' in i:
+            # 진눈깨비: 5
+            weather_code.append(5)
+        elif '눈' in i:
+            # 눈: 4
+            weather_code.append(4)
+        elif '소나기' in i or '비' in i:
+            # 비: 3
+            weather_code.append(3)
+        elif '흐' in i:
+            # 흐림: 2
+            weather_code.append(2)
+        elif '맑' in i or '줄어듦' in i:
+            # 맑음: 1
+            weather_code.append(1)
+        else:
+            # 아직 코드를 추가하지 않음. 나중에 프로그램이 커지면 알아서 하겠지
+            weather_code.append(0)
     
-print(weather_code)
-# pprint(set([i[0] for i in weather_list]))
+    return (temperature_list, weather_code)
+    
+
+if __name__ == '__main__':
+    pprint(get_predict_code())
 
 
 """
