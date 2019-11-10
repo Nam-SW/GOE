c7f0c78ad7ab6d21c290c8c21c93df5e3a377782
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-11-10 14:14:16 +0900

    아시바 라벨링 존나싫어;;;

--- AI/test.py
+++ AI/test.py
@@ -1,6 +1,25 @@
-import tensorflow as tf
-import numpy as np
+# import tensorflow as tf
+# import numpy as np
 
 
-load_data = np.load('./dataset/m1_input.npy')
-print(load_data)
+# load_data = np.load('./dataset/m1_input.npy')
+# print(load_data)
+
+import datetime
+def get_day(day):
+    day_len = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_code = ['월', '화', '수', '목', '금', '토', '일']
+
+    month = 1
+    for i in day_len:
+        if day > i:
+            month += 1
+            day -= i
+        else:
+            break
+    print(month, day)
+    return day_code[datetime.date(2018, month, day).weekday()]
+
+
+while True:
+    print(get_day(int(input())))
