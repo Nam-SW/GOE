5c1bf672d7f9831fa09538c0bb0233a41ac2996e
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-11-26 00:42:02 +0900

    학습 오류 수정

--- AI/app.py
+++ AI/app.py
@@ -63,7 +63,7 @@ def update():
     while True:
         if time.strftime('%H', time.localtime(time.time())) == '00':
         # if True:
-            yesterday = goe.get_yesterday_weather()
+            yesterday = goe.get_yesterday_weather() # 전날 변경
             db.reference(yesterday[0][:4]+'/weather').child(yesterday[0][4:]).set({
                 'Month': yesterday[1], 
                 'Day': yesterday[2], 
@@ -82,12 +82,22 @@ def update():
                     'Temperature': data[2],
                     'Weather': data[3]
                 })
+            print('예측 업데이트 완료')
 
-            data = db.reference(yesterday[0][:4]+'/electricity_use/'+yesterday[0][4:]).get()
+
+            data = db.reference(yesterday[0][:4]+'/electricity_use/'+yesterday[0][4:]).get() # 모델 학습
+            print(data)
             x = yesterday[1:3] + yesterday[3:]
-            y = [data[i]['airconditioner'] for i in data.keys()] + [data[i]['refrigerator'] for i in data.keys()]
+            y = []
+            for k in data[0].keys():
+                for i in data:
+                    y.append(i[k])
+                    
+            # print(x)
+            # print(y)
 
-            # goe.weather_predict()
+            goe.model_fit(x, y)
+            print('모델 학습 완료')
         time.sleep(3600)
 
 if __name__ == '__main__':
