667abe878c1221d0842d7e9597903b5bacc38f2e
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-11-25 17:30:41 +0900

    아마도 앱 최종

--- AI/app.py
+++ AI/app.py
@@ -71,6 +71,6 @@ def update():
 
 if __name__ == '__main__':
     threading.Thread(target=update).start()
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=8080)
     
     
