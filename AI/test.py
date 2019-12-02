7cf6d3de7a93c1b7d9a1411697f7d8eb2bd38e41
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-12-02 17:40:45 +0900

    sd

--- AI/test.py
+++ AI/test.py
@@ -2,7 +2,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from GOE_AI import GOE
-# from weather_data import get_temperature, get_weather_code, get_day
+from weather_data import get_temperature, get_weather_code, get_day
+from weather_predict import get_predict_code
 
 DB_link='https://guardians-of-energy.firebaseio.com/'
 DB_cred='./GOE.json'
@@ -11,16 +12,12 @@ firebase_admin.initialize_app(credentials.Certificate(DB_cred), {
     'databaseURL' : DB_link
 })
 
-# print(db.reference('2019/electricity_use/1100').get())
 
-# ref = db.reference()
-# print(ref.get())
-
-# goe = GOE()
-# t, w = get_temperature(2019, 11), get_weather_code(2019, 11) # 11월 데이터 등록
+goe = GOE()
+# t, w = get_temperature(2019, 12), get_weather_code(2019, 12) # 11월 데이터 등록
 # ref = db.reference('2019/weather')
 # for d in range(len(t)):
-#     k = "11%02d"%(d+1)
+#     k = "12%02d"%(d+1)
 #     print(d, k)
 #     d_ref = ref.child(k)
 #     d_ref.set({
@@ -43,28 +40,45 @@ firebase_admin.initialize_app(credentials.Certificate(DB_cred), {
 
 
 # import numpy as np
-# x = np.load('./dataset/m1_Xtrain.npy')
 # y = np.load('./dataset/m1_Ytrain.npy')
 
 # ref = db.reference('2019/electricity_use')
-# for i, k in zip(range(304, 329), range(1, 26)):
+# for i, k in zip(range(304, 334), range(1, 31)): # 11월 1일~30일
 #     key = '11%02d'%k
 #     print(key)
-    
 #     test = ref.child(key)
-
 #     test.set({k1:{k2:int(y[i][k1+i2]) for k2, i2 in zip(['airconditioner', 'refrigerator'], [0, 48])} for k1 in range(48)})
 
-import datetime, time
+ref = db.reference(goe.get_today()[:4]+'/weather')
 while True:
-    now = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
-    print(now[:4]+'/electricity_use/'+now[4:8] + str(int(now[8:10])*2 + (int(now[10:]) // 30)))
-    if now[8:] == '0000':
-        db.reference(now[:4]+'/electricity_use').child(now[4:8]).set({})
-        print(now + ' : mk table')
-    if now[10:] in ['00', '30']:
-        key = int(now[8:10])*2 + (int(now[10:]) // 30)
-        # print(key)
-        db.reference(now[:4]+'/electricity_use/'+now[4:8]).child(key).set({'airconditioner':0, 'refrigerator':1})
-        print(now + ' : update')
-    time.sleep(60)
+    try:
+        for key, data in zip(goe.remaining_day(), goe.weather_predict()): # 이번달 예측
+            # print(key, data)
+            d_ref = ref.child(key)
+            d_ref.set({
+                'Month': data[0],
+                'Day': data[1],
+                'Temperature': data[2],
+                'Weather': data[3]
+                })
+        break
+    except:
+        print('error')
+
+# import datetime, time
+# while True:
+#     now = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+#     # print(now[:4]+'/electricity_use/'+now[4:8] + str(int(now[8:10])*2 + (int(now[10:]) // 30)))
+
+#     if now[8:] == '0000':
+#         db.reference(now[:4]+'/electricity_use').child(now[4:8]).set({0:{'airconditioner':0, 'refrigerator':1}})
+#         print(now + ' : mk table')
+#         time.sleep(60)
+#         continue
+    
+#     if now[10:] in ['00', '30']:
+#         key = str(int(now[8:10])*2 + (int(now[10:]) // 30))
+#         print(now[:4]+'/electricity_use/'+now[4:8]+'/', key)
+#         db.reference(now[:4]+'/electricity_use/'+now[4:8]).child(key).set({'airconditioner':0, 'refrigerator':1})
+#         print(now + ' : update')
+#     time.sleep(60)
